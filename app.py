5a5d695556fbbf672d2e016cae48426e329cdfdd
Saroj Paudel <sarozpaudel6@gmail.com>
2025-06-04 14:00:10 +0545

    little change

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 
 app = Flask(__name__)
 model = load_model('models/tomatoes.h5')
+
+
+classes=['Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato__Target_Spot',
+ 'Tomato__Tomato_mosaic_virus',
+ 'Tomato_healthy']
+
+MODEL = tf.keras.models.load_model("models/tomatoes.h5")
 
 @app.route('/')
 def index():
@@ -19,9 +31,10 @@ def predict():
     img_loaded = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img_loaded) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)
+    prediction = MODEL.predict(img_array)
     class_idx = np.argmax(prediction)
-    return f"Disease class predicted: {class_idx}"
+    detected_class=classes[class_idx]
+    return f"Disease class predicted: {detected_class}"
 
 
 if __name__ == '__main__':
