774b6a473c7e23344b56c5d1dc8c0c60b624c7ff
Saroj Paudel <sarozpaudel6@gmail.com>
2025-06-05 08:42:31 +0545

    image array fix

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
 # model = load_model('models/tomatoes.h5')
@@ -19,6 +21,12 @@ classes=['Tomato_Bacterial_spot',
 
 MODEL = tf.keras.models.load_model("models/tomatoes.h5")
 
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -26,13 +34,15 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     img = request.files['file']
-    img_loaded = image.load_img(img, target_size=(224, 224))
-    img_array = image.img_to_array(img_loaded) / 255.0
+    img_bytes = img.read()
+    img_loaded = read_file_as_image(img_bytes)
+    img_resized = Image.fromarray(img_loaded).resize((224, 224))  # Adjust size as per your model input
+    img_array = np.array(img_resized) / 255.0  # Normalize if model expects normalized input
     img_array = np.expand_dims(img_array, axis=0)
     prediction = MODEL.predict(img_array)
     class_idx = np.argmax(prediction)
     detected_class = classes[class_idx]
-    return f"Disease class predicted: {detected_class}"
+    return f"Disease class predicted: {detected_class}, Accuracy:{np.max(prediction)}"
 
 
 if __name__ == '__main__':
